5459bcb84159d5e6371f3b18b993b2305d749fc6
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-06 15:34:26 +0200

    Add User entity with validation and JPA annotations.

---
+++ src/main/java/com/sproutsync/userservice/model/User.java
@@ -0,0 +1,35 @@
+package com.sproutsync.userservice.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 25)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 25)
+    private String surname;
+
+    @Email
+    @NotBlank
+    @Column(unique = true)
+    private String email;
+
+    @Size(min = 8, max = 100)
+    private String password;
+}
