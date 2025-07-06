80a356c8ebe8d457ad9e662db58b85387cb0be29
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-06 14:08:51 +0200

    first commit

---
+++ src/test/java/com/sproutsync/userservice/SproutSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sproutsync.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SproutSyncApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
