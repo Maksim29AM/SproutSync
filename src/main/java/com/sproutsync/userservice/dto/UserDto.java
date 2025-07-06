bdf05a655cea29a1bdaac7cd58a9d969eb345ee4
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-06 15:35:19 +0200

    Add UserDto with validation annotations for input data.

---
+++ src/main/java/com/sproutsync/userservice/dto/UserDto.java
@@ -0,0 +1,24 @@
+package com.sproutsync.userservice.dto;
+
+import jakarta.validation.constraints.*;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+
+    @NotBlank
+    @Size(min = 3, max = 25)
+    private String username;
+
+    @NotBlank
+    @Size(min = 3, max = 25)
+    private String surname;
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @Size(min = 8, max = 100)
+    private String password;
+}
