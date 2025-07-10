0d4bb4efe057645ca60f10d85fd9febeecdbf152
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-10 20:05:47 +0200

    Replace field injection with constructor injection.

--- src/main/java/com/sproutsync/userservice/service/MyUserDetailsService.java
+++ src/main/java/com/sproutsync/userservice/service/MyUserDetailsService.java
@@ -3,7 +3,6 @@ package com.sproutsync.userservice.service;
 import com.sproutsync.userservice.model.User;
 import com.sproutsync.userservice.model.UserPrincipal;
 import com.sproutsync.userservice.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -12,8 +11,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class MyUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+
+    private final UserRepository userRepository;
+
+    public MyUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
