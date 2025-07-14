6dd761be106fdd6b5ab553539686a920127673ab
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-14 11:43:08 +0200

    Refactor table name.

--- src/main/java/com/sproutsync/userservice/model/Group.java
+++ src/main/java/com/sproutsync/userservice/model/Group.java
@@ -1,9 +1,6 @@
 package com.sproutsync.userservice.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,6 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "child_group")
 public class Group {
 
     @Id
