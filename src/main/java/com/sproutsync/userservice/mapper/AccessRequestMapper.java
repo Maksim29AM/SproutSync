579f5f37de9db5309817a945641594a3ea86391d
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-15 12:26:52 +0200

    Add AccessRequestMapper for converting between entity and DTO.

---
+++ src/main/java/com/sproutsync/userservice/mapper/AccessRequestMapper.java
@@ -0,0 +1,34 @@
+package com.sproutsync.userservice.mapper;
+
+import com.sproutsync.userservice.dto.AccessRequestDto;
+import com.sproutsync.userservice.model.AccessRequest;
+import com.sproutsync.userservice.model.Group;
+import com.sproutsync.userservice.model.User;
+import com.sproutsync.userservice.util.AccessStatus;
+
+import java.time.LocalDateTime;
+
+public class AccessRequestMapper {
+
+    private AccessRequestMapper() {
+    }
+
+    public static AccessRequest toEntity(User user, Group group) {
+        return new AccessRequest(
+                null,
+                user,
+                group,
+                AccessStatus.PENDING,
+                LocalDateTime.now(),
+                null);
+    }
+
+    public static AccessRequestDto toDto(AccessRequest accessRequest) {
+        AccessRequestDto dto = new AccessRequestDto();
+        dto.setId(accessRequest.getId());
+        dto.setUserId(accessRequest.getParent().getId());
+        dto.setGroupId(accessRequest.getGroup().getId());
+        dto.setAccessStatus(accessRequest.getAccessStatus());
+        return dto;
+    }
+}
