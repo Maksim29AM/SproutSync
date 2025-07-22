e76561de3725d33068d259574f2bd5b54445e7d1
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-07-22 21:29:32 +0200

    Add Menu entity.

---
+++ src/main/java/com/sproutsync/userservice/model/Menu.java
@@ -0,0 +1,42 @@
+package com.sproutsync.userservice.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", referencedColumnName = "id", nullable = false)
+    private Group group;
+
+    private LocalDate date;
+
+    private String meals;
+
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+
+
+    @PrePersist
+    public void prePersist() {
+        createdAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
