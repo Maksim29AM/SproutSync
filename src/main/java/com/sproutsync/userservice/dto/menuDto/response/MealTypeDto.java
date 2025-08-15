ebe0ca6b317e94ae65dbd068c716e10d579648c9
Maksim29AM <maks.abramovich.1995@mail.ru>
2025-08-15 22:04:14 +0200

    Rename `MealTypeDto` package to `dto.menuDto.response`

--- src/main/java/com/sproutsync/userservice/dto/menuDto/response/MealTypeDto.java
+++ src/main/java/com/sproutsync/userservice/dto/menuDto/response/MealTypeDto.java
@@ -1,10 +1,9 @@
-package com.sproutsync.userservice.dto;
+package com.sproutsync.userservice.dto.menuDto.response;
 
 import lombok.Data;
 
 @Data
 public class MealTypeDto {
-
 
     private String name;
 
